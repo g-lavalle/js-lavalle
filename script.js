714782b98c5b629b879a7ed06c9667c867f28e19
g-lavalle <glavalle@fi.uba.ar>
2024-08-14 18:54:50 -0300

    Comento el codigo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Define un array con los productos disponibles
+// Defino un array con los productos disponibles, le da escalabilidad a la pagina
 const productos = [
     { id: 1, nombre: "Análisis numérico", precio: 10000, descripcion: "Burden", imagen: "img/analisis-numerico.jpeg", departamento:"Matematica" },
     { id: 2, nombre: "Álgebra Lineal 2", precio: 15000, descripcion: "Exactas UBA", imagen: "img/algebra-lineal-2.png", departamento:"Matematica" },
@@ -10,19 +10,18 @@ const productos = [
     { id: 8, nombre: "Operaciones", precio: 20000, descripcion: "Render", imagen: "img/operaciones.jpg", departamento:"Gestion" }
 ];
 
-// Inicializa el carrito a partir de sessionStorage, si existe. Si no, se crea un carrito vacío.
 let carrito = JSON.parse(sessionStorage.getItem('carrito')) || [];
 
-// Muestra los productos en la página
+// Muestro los productos en la página
 function mostrarProductos() {
     const contenedorProductos = document.getElementById('contenedor-productos');
 
     productos.forEach(producto => {
-        // Crea un div para cada producto
+        // Creo un div para cada producto
         const productoDiv = document.createElement('div');
         productoDiv.classList.add('producto');
 
-        // Inserta el HTML para mostrar el producto incluyendo la imagen
+        // Inserto el HTML
         productoDiv.innerHTML = `
             <img src="${producto.imagen}" alt="${producto.nombre}" class="imagen-producto">
             <h3>${producto.nombre}</h3>
@@ -34,34 +33,32 @@ function mostrarProductos() {
             <button onclick="agregarAlCarrito(${producto.id}, ${producto.precio})">Agregar al Carrito</button>
         `;
 
-        // Añade el div al contenedor de productos en el DOM
+        // Añado el div al contenedor de productos en el DOM
         contenedorProductos.appendChild(productoDiv);
     });
 }
 
 // Función para agregar productos al carrito
 function agregarAlCarrito(productoId, precio) {
-    // Obtiene la cantidad seleccionada por el usuario
+    // Obtengo la cantidad seleccionada por el usuario
     const cantidadInput = document.getElementById(`producto${productoId}-cantidad`);
     const cantidad = parseInt(cantidadInput.value);
 
-    // Si la cantidad es mayor a 0, se procede a agregar al carrito
+    // Si la cantidad es mayor a 0, se agrega al carrito
     if (cantidad > 0) {
         // Busca si el producto ya está en el carrito
         const productoEnCarrito = carrito.find(item => item.id === productoId);
 
         if (productoEnCarrito) {
-            // Si el producto ya está en el carrito, se suma la cantidad
+            // Si el producto ya está en el carrito, sumo la cantidad
             productoEnCarrito.cantidad += cantidad;
         } else {
-            // Si el producto no está en el carrito, se agrega como un nuevo objeto
+            // Si el producto no está en el carrito, lo agrego como un nuevo objeto
             carrito.push({ id: productoId, precio: precio, cantidad: cantidad });
         }
 
-        // Guarda el carrito actualizado en sessionStorage
         sessionStorage.setItem('carrito', JSON.stringify(carrito));
 
-        // Actualiza la visualización del carrito
         actualizarCarrito();
     }
 }
@@ -71,25 +68,24 @@ function actualizarCarrito() {
     let cantidadCarrito = 0;  // Contador para la cantidad total de productos en el carrito
     let totalCarrito = 0;     // Acumulador para el costo total del carrito
 
-    // Recorre cada producto en el carrito para sumar cantidades y calcular el total
+    // Recorro cada producto en el carrito para sumar cantidades y calcular el total
     carrito.forEach(producto => {
         cantidadCarrito += producto.cantidad;
         totalCarrito += producto.precio * producto.cantidad;
     });
 
-    // Actualiza el DOM para mostrar la cantidad total de productos y el costo total
     document.getElementById('cantidad-carrito').innerText = cantidadCarrito;
     document.getElementById('total-carrito').innerText = totalCarrito.toFixed(2); // Redondea a 2 decimales
 }
 
 document.getElementById('finalizar-compra').addEventListener('click', () => {
-    // Calcula el total
+
     const totalCompra = document.getElementById('total-carrito').innerText;
 
-    // Muestra un mensaje de confirmación en la página
+    // Muestro un mensaje de confirmación en la página
     const mensajeConfirmacion = document.getElementById('mensaje-confirmacion');
     mensajeConfirmacion.innerText = `¡Gracias por tu compra! El total de tu compra es $${totalCompra}.`;
-    mensajeConfirmacion.style.display = 'block'; // Muestra el mensaje
+    mensajeConfirmacion.style.display = 'block'; // Muestra el mensaje, por default en el HTML esta con display none
 
     // Limpia el carrito
     sessionStorage.removeItem('carrito');
@@ -97,8 +93,8 @@ document.getElementById('finalizar-compra').addEventListener('click', () => {
     actualizarCarrito();
 });
 
-// Mostrar productos en la página cuando se carga
+// Cargo programa
 mostrarProductos();
 
-// Actualizar la visualización del carrito desde sessionStorage cuando se carga la página
+// Actualizo la visualización del carrito desde sessionStorage cuando se carga la página
 actualizarCarrito();
